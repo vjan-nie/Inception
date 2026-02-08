ab5d437237bee2fab4adcc5d6b5b023f9155839a
Vado <vjannieto@gmail.com>
2026-02-08 12:57:21 +0100

    mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 set -e
 
-# Paths & permissions
-mkdir -p /run/mysqld /var/lib/mysql
-chown -R mysql:mysql /run/mysqld /var/lib/mysql
+# Paths & permissions if database not initialized
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
 
-# Backgorund start of MariaDB as mysql
-mysqld_safe --user=mysql &
+  # Explicit volume permissions in case they changed
+  chown -R mysql:mysql /var/lib/mysql
 
-# Wait until it's ready
-until mysql -u root -e "SELECT 1;" >/dev/null 2>&1; do
-  echo "Waiting for MariaDB to start..."
-  sleep 2
-done
+  # Backgorund start of MariaDB as mysql
+  mysqld_safe --user=mysql --skip-networking &
 
-# Create Data Base & Users
-mysql -u root <<EOF
+  # Wait until it's ready
+  until mysqladmin ping >/dev/null 2>&1; do
+    echo "Waiting for MariaDB to start..."
+    sleep 2
+  done
+
+  # Create Data Base & Users
+  mysql -u root <<EOF
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
@@ -23,9 +25,11 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
 
-# Stop background MariaDB, and usenew set password
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-sleep 2
+  # Stop background MariaDB, and usenew set password
+  mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+  sleep 2
+
+fi
 
 # Execute original CMD from Dockerfile, which will become PID1
 exec mysqld --user=mysql
